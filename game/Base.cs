af734e984de06e2571a21a69ad9ace4bd9487292
Henry <2280825018@qq.com>
2025-03-06 21:14:36 +0800

    feat: CharacterWindowsUpdates

---
+++ game/Base.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+namespace Basicsettings;
+public static class Base
+{
+	public static bool[] CharacterVisibility = new bool [9] { true, true, true, true, true, true, true, false, false };
+}
---
+++ game/GameEnum.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+namespace Basicsettings;
+public static class GameEnum
+{
+	public enum characterEnum
+	{
+		JinShichang = 0,
+		HuangWanrong = 1,
+		JinYiyi = 2,
+		ZhouMingyuan = 3,
+		LiSong = 4,
+		LiuQianqian = 5,
+		SongYan = 6,
+		SunJiadong = 7,
+		WangXiangyu = 8,
+		ChunyuShiyan = 9,
+	}
+}
